3a34f325b61da26e3c54d378516b4580a9fa9d4c
KranK - ズ丂ﾑ刀ズ <Krankshuffle@outlook.com>
2019-02-24 22:51:43 +0200

    Upload sourcecode

---
+++ EX BootLogo/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace EX_BootLogo
+{
+    partial class Load_Image_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Load_Image_Form));
+            this.LoadImageDilaog = new System.Windows.Forms.OpenFileDialog();
+            this.SaveBootLogoDialog = new System.Windows.Forms.SaveFileDialog();
+            this.LoadedImageBox = new System.Windows.Forms.PictureBox();
+            this.LoadImageButton = new MaterialSkin.Controls.MaterialFlatButton();
+            this.SaveBootLogoButton = new MaterialSkin.Controls.MaterialFlatButton();
+            this.IconBox = new System.Windows.Forms.PictureBox();
+            this.ConfrimSizeLabel = new MaterialSkin.Controls.MaterialLabel();
+            this.HeightLabel = new MaterialSkin.Controls.MaterialLabel();
+            this.WidthLabel = new MaterialSkin.Controls.MaterialLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.LoadedImageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IconBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LoadedImageBox
+            // 
+            this.LoadedImageBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.LoadedImageBox.Cursor = System.Windows.Forms.Cursors.Help;
+            this.LoadedImageBox.Image = global::EX_BootLogo.Properties.Resources.No_Image;
+            this.LoadedImageBox.Location = new System.Drawing.Point(192, 76);
+            this.LoadedImageBox.Name = "LoadedImageBox";
+            this.LoadedImageBox.Size = new System.Drawing.Size(180, 320);
+            this.LoadedImageBox.TabIndex = 1;
+            this.LoadedImageBox.TabStop = false;
+            // 
+            // LoadImageButton
+            // 
+            this.LoadImageButton.AutoSize = true;
+            this.LoadImageButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.LoadImageButton.BackColor = System.Drawing.SystemColors.Control;
+            this.LoadImageButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.LoadImageButton.Depth = 0;
+            this.LoadImageButton.Location = new System.Drawing.Point(46, 76);
+            this.LoadImageButton.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.LoadImageButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.LoadImageButton.Name = "LoadImageButton";
+            this.LoadImageButton.Primary = false;
+            this.LoadImageButton.Size = new System.Drawing.Size(93, 36);
+            this.LoadImageButton.TabIndex = 3;
+            this.LoadImageButton.Text = "LOAD IMAGE";
+            this.LoadImageButton.UseVisualStyleBackColor = false;
+            this.LoadImageButton.Click += new System.EventHandler(this.LoadImageButton_Click);
+            // 
+            // SaveBootLogoButton
+            // 
+            this.SaveBootLogoButton.AutoSize = true;
+            this.SaveBootLogoButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.SaveBootLogoButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.SaveBootLogoButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SaveBootLogoButton.Depth = 0;
+            this.SaveBootLogoButton.Location = new System.Drawing.Point(23, 360);
+            this.SaveBootLogoButton.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.SaveBootLogoButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.SaveBootLogoButton.Name = "SaveBootLogoButton";
+            this.SaveBootLogoButton.Primary = false;
+            this.SaveBootLogoButton.Size = new System.Drawing.Size(139, 36);
+            this.SaveBootLogoButton.TabIndex = 3;
+            this.SaveBootLogoButton.Text = "EXPORT BOOTLOGO";
+            this.SaveBootLogoButton.UseVisualStyleBackColor = false;
+            this.SaveBootLogoButton.Click += new System.EventHandler(this.SaveBootLogoButton_Click);
+            // 
+            // IconBox
+            // 
+            this.IconBox.Image = global::EX_BootLogo.Properties.Resources.EXBootIcon_;
+            this.IconBox.Location = new System.Drawing.Point(2, 3);
+            this.IconBox.Name = "IconBox";
+            this.IconBox.Size = new System.Drawing.Size(16, 16);
+            this.IconBox.TabIndex = 4;
+            this.IconBox.TabStop = false;
+            // 
+            // ConfrimSizeLabel
+            // 
+            this.ConfrimSizeLabel.AutoSize = true;
+            this.ConfrimSizeLabel.Depth = 0;
+            this.ConfrimSizeLabel.Font = new System.Drawing.Font("Roboto", 11F);
+            this.ConfrimSizeLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.ConfrimSizeLabel.Location = new System.Drawing.Point(30, 173);
+            this.ConfrimSizeLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.ConfrimSizeLabel.Name = "ConfrimSizeLabel";
+            this.ConfrimSizeLabel.Size = new System.Drawing.Size(124, 19);
+            this.ConfrimSizeLabel.TabIndex = 5;
+            this.ConfrimSizeLabel.Text = "CONFRIM SIZE !?";
+            // 
+            // HeightLabel
+            // 
+            this.HeightLabel.AutoSize = true;
+            this.HeightLabel.Cursor = System.Windows.Forms.Cursors.Help;
+            this.HeightLabel.Depth = 0;
+            this.HeightLabel.Font = new System.Drawing.Font("Roboto", 11F);
+            this.HeightLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.HeightLabel.Location = new System.Drawing.Point(36, 208);
+            this.HeightLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.HeightLabel.Name = "HeightLabel";
+            this.HeightLabel.Size = new System.Drawing.Size(61, 19);
+            this.HeightLabel.TabIndex = 6;
+            this.HeightLabel.Text = "Height: ";
+            // 
+            // WidthLabel
+            // 
+            this.WidthLabel.AutoSize = true;
+            this.WidthLabel.Cursor = System.Windows.Forms.Cursors.Help;
+            this.WidthLabel.Depth = 0;
+            this.WidthLabel.Font = new System.Drawing.Font("Roboto", 11F);
+            this.WidthLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.WidthLabel.Location = new System.Drawing.Point(41, 239);
+            this.WidthLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.WidthLabel.Name = "WidthLabel";
+            this.WidthLabel.Size = new System.Drawing.Size(55, 19);
+            this.WidthLabel.TabIndex = 6;
+            this.WidthLabel.Text = "Width: ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(21, 358);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 38);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.SaveBootLogoButton_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(45, 74);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(102, 38);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.LoadImageButton_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Help;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(192, 74);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(188, 320);
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Load_Image_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(413, 418);
+            this.Controls.Add(this.LoadedImageBox);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.LoadImageButton);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.SaveBootLogoButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.WidthLabel);
+            this.Controls.Add(this.HeightLabel);
+            this.Controls.Add(this.ConfrimSizeLabel);
+            this.Controls.Add(this.IconBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Load_Image_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EX BootLogo - Nintendo Switch Bootlogo Converter";
+            this.Load += new System.EventHandler(this.Load_Image_Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.LoadedImageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IconBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog LoadImageDilaog;
+        private System.Windows.Forms.PictureBox LoadedImageBox;
+        private System.Windows.Forms.SaveFileDialog SaveBootLogoDialog;
+        private MaterialSkin.Controls.MaterialFlatButton LoadImageButton;
+        private System.Windows.Forms.PictureBox IconBox;
+        private MaterialSkin.Controls.MaterialLabel ConfrimSizeLabel;
+        private MaterialSkin.Controls.MaterialLabel HeightLabel;
+        private MaterialSkin.Controls.MaterialLabel WidthLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private MaterialSkin.Controls.MaterialFlatButton SaveBootLogoButton;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ EX BootLogo/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace EX_BootLogo
+{
+    public partial class Load_Image_Form : MaterialForm
+    {
+        public Load_Image_Form()
+        {
+            InitializeComponent();
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Orange100, Primary.Orange300, Primary.Orange800, Accent.DeepOrange700, TextShade.BLACK);
+        }
+       
+        Bitmap Creat_BootLogo(string input)
+        {
+            Bitmap ImageInput = new Bitmap(input);
+            Bitmap ImageOutput = new Bitmap(ImageInput.Width, ImageInput.Height, PixelFormat.Format32bppArgb);
+            Graphics ImageHandle = Graphics.FromImage(ImageOutput);
+            ImageHandle.DrawImage(ImageInput, new Rectangle(0, 0, ImageOutput.Width, ImageOutput.Height));
+            ImageHandle.Dispose();
+            return ImageOutput;
+        }
+
+        private void LoadImageButton_Click(object sender, EventArgs e)
+        {
+            LoadImageDilaog.Filter = "Image Files (*.bmp;*.jpg;*.jpeg,*.png)|*.BMP;*.JPG;*.JPEG;*.PNG";
+            if (LoadImageDilaog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    Bitmap Image_var = new Bitmap(LoadImageDilaog.FileName);
+                    HeightLabel.Text = HeightLabel.Text + Convert.ToString(Image_var.Height);
+                    WidthLabel.Text = WidthLabel.Text + Convert.ToString(Image_var.Width);
+                    LoadedImageBox.ImageLocation = LoadImageDilaog.FileName;
+                    LoadedImageBox.SizeMode = PictureBoxSizeMode.CenterImage;
+                    LoadedImageBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                    MessageBox.Show("Image Loaded :)", "OK !", MessageBoxButtons.OK);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Cant Load Image :(\nTry again!", "Ohh !", MessageBoxButtons.OK);
+                    return;
+                }
+               
+            }
+        }
+
+        private void Load_Image_Form_Load(object sender, EventArgs e)
+        {
+            LoadedImageBox.SizeMode = PictureBoxSizeMode.CenterImage;
+            LoadedImageBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            IconBox.SizeMode = PictureBoxSizeMode.CenterImage;
+            IconBox.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void SaveBootLogoButton_Click(object sender, EventArgs e)
+        {
+            SaveBootLogoDialog.Filter = "Hekate/Atmosphere bootlogo|*.BMP";
+            SaveBootLogoDialog.FileName = "bootlogo.bmp";
+            if (SaveBootLogoDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    Creat_BootLogo(LoadImageDilaog.FileName).Save(SaveBootLogoDialog.FileName, ImageFormat.Bmp);
+                    MessageBox.Show("Boot Logo Exported :)", "OK !", MessageBoxButtons.OK);
+                    
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Error Exporting Logo :(\nTry again!", "Ohh!", MessageBoxButtons.OK);
+                    return;
+                }
+                
+            }
+        }
+    }
+}
